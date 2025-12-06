053c8136727fa72d5aec17e9637974855aa79891
alessandrobaldazzi <baldale98@gmail.com>
2025-12-06 21:11:47 +0100

    Added configuration options

--- CinemaniacToLetterboxd.py
+++ CinemaniacToLetterboxd.py
@@ -2,8 +2,10 @@ import json
 from datetime import datetime
 import csv
 import random
+import configparser
 
-def cinemaniac_to_list(filename):
+def cinemaniac_to_list(config):
+    filename = config.get('file-io','input')
     with open(filename, encoding="utf-8") as file:
         data = json.load(file)
     ratings = data['ratings']
@@ -11,6 +13,13 @@ def cinemaniac_to_list(filename):
 
     watched_movies = []
     watchlist = []
+
+    use_save_date_as_diary = config.get('options','use_save_date_as_diary') == 'True'
+    random_date_if_invalid = config.get('options','random_date_if_invalid') == 'True'
+    random_is_invalid_if_smaller = int(config.get('options','random_is_invalid_if_smaller'))
+    random_lower_date = int(config.get('options','random_lower_date'))
+    random_upper_date = int(config.get('options','random_upper_date'))
+
 
     for movie in movies:
         ## Remove unnecessary fields
@@ -32,26 +41,38 @@ def cinemaniac_to_list(filename):
             movie.pop('date')
             watchlist.append(movie)
         else:
-            ## If the date is not reasonable put random date between 2015 and 2020
-            if(movie['date'] < 1420070400000):
-                movie['date'] = random.randint(1420070400000, 1582070400000)
+            if(use_save_date_as_diary):
+                ## If the date is not reasonable put random date
+                if(random_date_if_invalid and movie['date'] < random_is_invalid_if_smaller):
+                    movie['date'] = random.randint(random_lower_date, random_upper_date)
 
-            ## Add watch date and rating to entry
-            movie['WatchedDate'] = datetime.fromtimestamp(movie.pop('date')/1000).strftime("%Y-%m-%d")
+                ## Add watch date
+                movie['WatchedDate'] = datetime.fromtimestamp(movie.pop('date')/1000).strftime("%Y-%m-%d")
+            else:
+                movie.pop('date')
+
+            ## Add rating to entry
             for rating in ratings:
                 if(rating['id'] == movie['tmdbID']):
                     movie['Rating10'] = rating['r']
                     ratings.remove(rating)
                     break
             watched_movies.append(movie)
+
     ## Save to file
-    with open('watched_movies.csv', 'w', newline='') as watched_file:
-        fieldnames = ["tmdbID", "WatchedDate", "Rating10"]
+    watched_filename = config.get('file-io','watched')
+    watchlist_filename = config.get('file-io','watchlist')
+
+    with open(watched_filename, 'w', newline='') as watched_file:
+        if(use_save_date_as_diary):
+            fieldnames = ["tmdbID", "WatchedDate", "Rating10"]
+        else:
+            fieldnames = ["tmdbID", "Rating10"]
         writer = csv.DictWriter(watched_file, fieldnames)
         writer.writeheader()
         writer.writerows(watched_movies)
     
-    with open('watchlist.csv', 'w', newline='') as watchlist_file:
+    with open(watchlist_filename, 'w', newline='') as watchlist_file:
         fieldnames = ["tmdbID"]
         writer = csv.DictWriter(watchlist_file, fieldnames)
         writer.writeheader()
@@ -59,9 +80,9 @@ def cinemaniac_to_list(filename):
 
 
 def main():
-    filename = "Cinemaniac.bak"
-    cinemaniac_to_list(filename)
-
+    config = configparser.ConfigParser()
+    config.read(r'config.cfg')
+    cinemaniac_to_list(config)
 
 if __name__ == "__main__":
     main()
